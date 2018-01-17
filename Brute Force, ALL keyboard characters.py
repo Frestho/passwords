8388c0ce36c66146eea19b7f967f2697ba2417bb
Frestho <34761944+Frestho@users.noreply.github.com>
2018-01-16 21:46:42 -0800

    Updated code

--- Brute Force, ALL keyboard characters.py
+++ Brute Force, ALL keyboard characters.py
@@ -1,6 +1,8 @@
+import time
 pw = input("Enter your password: ")
-cracked = false #state of password cracking
-chars = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890~!@#$%^&*()_+`-=[]\;',./{}|:"<>?" #all the characters the the program will try
+start = time.time()
+cracked = False #state of password cracking
+chars = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890~!@#$%^&*()_+`-=[]\;',./{}|:\"<>?" #all the characters the the program will try
 charOn = [-1] #holds all the character values
 attempts = 0
 while not cracked:
@@ -10,13 +12,13 @@ while not cracked:
     charOn[len(charOn) - 1] += 1
     
     #if any character in charOn is over the limit (in this case, ?), it switches it back to the beginning (a) and changes the previous character by 1
-    for i in range(0, len(charOn)-1):
-        if charOn[i] > len(chars):
+    for i in reversed(range(1, len(charOn))):
+        if charOn[i] > len(chars)-1:
             charOn[i] = 0
             charOn[i-1] += 1
-            
+        
     #if the first character is z, then change it back to a and add a new character
-    if charOn[0] > 26: 
+    if charOn[0] > len(chars)-1:
         charOn[0] = 0
         charOn.append(0)
         
@@ -24,10 +26,10 @@ while not cracked:
     attempt = ""
     
     #decodes charOn into a string and store it in attempt
-    for i in range(0, len(charOn)-1):
+    for i in range(0, len(charOn)):
         attempt += chars[charOn[i]]
     
     #check if we got the password
     if attempt == pw:
-        print("Your password was found! It is " + attempt + ", right? It took me " + attempts + " attempts to find it.")
-        unsolved = false
+        print("Your password was found! It is " + str(attempt) + ", right? It took me " + str(attempts) + " attempts to find it. It took " + str(round(time.time()-start, 2)) + " seconds.")
+        cracked = True
