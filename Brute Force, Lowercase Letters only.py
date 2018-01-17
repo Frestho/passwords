e9023493ce835f19f5b19daf712f61c7d9cb0a3a
Frestho <34761944+Frestho@users.noreply.github.com>
2018-01-16 21:35:32 -0800

    More bugs...
    Fixed more bugs, namely, the attempts timer being inaccurate because it takes into account the time it takes for the user to input, which it should not.

--- Brute Force, Lowercase Letters only.py
+++ Brute Force, Lowercase Letters only.py
@@ -1,6 +1,6 @@
 import time
-start = time.time()
 pw = input("Enter your password: ")
+start = time.time()
 cracked = False #state of password cracking
 chars = "qwertyuiopasdfghjklzxcvbnm" #all the characters the the program will try
 charOn = [-1] #holds all the character values
@@ -10,13 +10,13 @@ while not cracked:
     
     #changes the right-most character of the password that is being attempted
     charOn[len(charOn) - 1] += 1
-
+    
     #if any character in charOn is over the limit (in this case, z), it switches it back to the beginning (a) and changes the previous character by 1
     for i in reversed(range(1, len(charOn))):
         if charOn[i] > len(chars)-1:
             charOn[i] = 0
             charOn[i-1] += 1
-            
+        
     #if the first character is z, then change it back to a and add a new character
     if charOn[0] > len(chars)-1:
         charOn[0] = 0
@@ -28,7 +28,7 @@ while not cracked:
     #decodes charOn into a string and store it in attempt
     for i in range(0, len(charOn)):
         attempt += chars[charOn[i]]
-    print(attempt)
+    
     #check if we got the password
     if attempt == pw:
         print("Your password was found! It is " + str(attempt) + ", right? It took me " + str(attempts) + " attempts to find it. It took " + str(round(time.time()-start, 2)) + " seconds.")
