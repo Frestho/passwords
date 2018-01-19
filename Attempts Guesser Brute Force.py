022550d8e9b0713c38b515d4565e41c4c3a84a9a
Conics <34761944+Frestho@users.noreply.github.com>
2018-01-19 15:20:33 -0800

    Fixed math error

--- Attempts Guesser Brute Force.py
+++ Attempts Guesser Brute Force.py
@@ -18,7 +18,7 @@ for i in range(0, len(pw)):
 
 #decode
 for i in range(0, len(charOn)):
-    attempts += charOn[i]*(len(chars)**(len(charOn) - i - 1)) + 1
+    attempts += (charOn[i] + 1) * (len(chars) ** (len(charOn) - i - 1)) + 1
 print("It would take " + str(attempts) + " attempts for your password to be cracked.")
 benchmark = input("To calculate how much time it would take for your password to be cracked, run my original brute force cracking program for about a minute and check the value of the attempts variable. Enter its value here: ")
 print("It would take " + str(round(int(benchmark) * 60 * attempts, 2)) + " seconds for your password to be cracked. This is only an approximation.")
