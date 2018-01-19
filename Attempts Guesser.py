5081e603d2e63f6d980c70089c66db5bc859bed4
Frestho <34761944+Frestho@users.noreply.github.com>
2018-01-18 20:53:51 -0800

    Math errors

--- Attempts Guesser.py
+++ Attempts Guesser.py
@@ -15,7 +15,7 @@ for i in range(0, len(pw)):
 
 #decode
 for i in range(0, len(charOn)):
-    attempts += charOn[i]*(len(chars)**len(charOn)-i)
+    attempts += charOn[i]*(len(chars)**(len(charOn) - i - 1)) + 1
 print("It would take " + str(attempts) + " attempts for your password to be cracked.")
 benchmark = input("To calculate how much time it would take for your password to be cracked, run my original brute force cracking program for about a minute and check the value of the attempts variable. Enter its value here: ")
-print("It would take " + str(int(benchmark) / 60 / attempts) + " seconds for your password to be cracked. This is only an approximation.")
+print("It would take " + str(int(benchmark) / 60 * attempts) + " seconds for your password to be cracked. This is only an approximation.")
