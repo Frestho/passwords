9c0b0c1ed4f3e59ebf3e312ec519625094ea3579
Frestho <34761944+Frestho@users.noreply.github.com>
2018-01-18 22:06:42 -0800

    New feature
    Measurements besides seconds, fixed some more math errors.

--- Attempts Guesser.py
+++ Attempts Guesser.py
@@ -1,3 +1,4 @@
+import math
 charOn = []
 mode = -1
 while not (mode == "" or mode == "0" or mode == "1"):
@@ -20,4 +21,5 @@ for i in range(0, len(charOn)):
     attempts += charOn[i]*(len(chars)**(len(charOn) - i - 1)) + 1
 print("It would take " + str(attempts) + " attempts for your password to be cracked.")
 benchmark = input("To calculate how much time it would take for your password to be cracked, run my original brute force cracking program for about a minute and check the value of the attempts variable. Enter its value here: ")
-print("It would take " + str(round(int(benchmark) / 60 * attempts, 2)) + " seconds for your password to be cracked. This is only an approximation.")
+print("It would take " + str(round(int(benchmark) * 60 * attempts, 2)) + " seconds for your password to be cracked. This is only an approximation.")
+print("It would take " + str(math.floor((int(benchmark) * 60 * attempts) / 86400)) + " days, " + str(math.floor((int(benchmark) * 60 * attempts) % 86400 / 3600)) + "hours, " + str(math.floor((int(benchmark) * 60 * attempts) % 3600 / 60)) + " minutes, and " + str(round((int(benchmark) * 60 * attempts) % 60, 2)) + " seconds for your password to be cracked. This is only an approximation.")
