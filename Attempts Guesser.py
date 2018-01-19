280a5b801bc76fea20e8385bdc1cb3121dc7a3b0
Frestho <34761944+Frestho@users.noreply.github.com>
2018-01-18 18:32:37 -0800

    Fixed all bugs

--- Attempts Guesser.py
+++ Attempts Guesser.py
@@ -4,17 +4,18 @@ if mode == "":
     chars = "qwertyuiopasdfghjklzxcvbnm"
 if mode == "0":
     chars = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890"
-if mode = "1":
+if mode == "1":
     chars = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890~!@#$%^&*()_+`-=[]\;',./{}|:\"<>?"
 pw = input("Enter your password here: ")
+attempts = 0
 
 #encode
 for i in range(0, len(pw)):
-    charOn.push(chars.index(pw[i]))
+    charOn.append(chars.index(pw[i]))
 
 #decode
 for i in range(0, len(charOn)):
     attempts += charOn[i]*(26**len(charOn)-i)
 print("It would take " + str(attempts) + " attempts for your password to be cracked.")
 benchmark = input("To calculate how much time it would take for your password to be cracked, run my original brute force cracking program for about a minute and check the value of the attempts variable. Enter its value here: ")
-print("It would take " + str(attempts * benchmark / 60) + " seconds for your password to be cracked. This is only an approximation.")
+print("It would take " + str(int(benchmark) / 60 / attempts) + " seconds for your password to be cracked. This is only an approximation.")
