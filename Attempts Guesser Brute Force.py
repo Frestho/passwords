0558312251e8ddc78c31c57bf92c1281f4578580
Frestho <34761944+Frestho@users.noreply.github.com>
2018-01-20 17:38:47 -0800

    Correct letter orders

--- Attempts Guesser Brute Force.py
+++ Attempts Guesser Brute Force.py
@@ -4,11 +4,11 @@ mode = -1
 while not (mode == "" or mode == "0" or mode == "1"):
     mode = input("Select the mode you want to test. Nothing for all lowercase, 0 for all case letters + numbers, and 1 for all characters: ")
     if mode == "":
-        chars = "qwertyuiopasdfghjklzxcvbnm"
+        chars = "aoeuidhtnspyfgcrlqjkxbmwvz"
     if mode == "0":
-        chars = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890"
+        chars = "aoeuidhtnspyfgcrlqjkxbmwvzAOEUIDHTNSPYFGCRLQJKXBMWVZ1234567890"
     if mode == "1":
-        chars = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890~!@#$%^&*()_+`-=[]\;',./{}|:\"<>?"
+        chars = "aoeuidhtnspyfgcrlqjkxbmwvzAOEUIDHTNSPYFGCRLQJKXBMWVZ1234567890~!@#$%^&*()_+`-=[]\;',./{}|:\"<>?"
 pw = input("Enter your password here: ")
 attempts = 0
 
@@ -18,7 +18,7 @@ for i in range(0, len(pw)):
 
 #decode
 for i in range(0, len(charOn)):
-    attempts += (charOn[i] + 1) * (len(chars) ** (len(charOn) - i - 1)) + 1
+    attempts += (charOn[i] + 1) * (len(chars) ** (len(charOn) - i - 1))
 print("It would take " + str(attempts) + " attempts for your password to be cracked.")
 benchmark = input("To calculate how much time it would take for your password to be cracked, run my original brute force cracking program for about a minute and check the value of the attempts variable. Enter its value here: ")
 print("It would take " + str(round(60 / int(benchmark) * attempts, 2)) + " seconds for your password to be cracked. This is only an approximation.")
