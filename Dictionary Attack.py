1e303e2f1c97588475ec07fbc6771458cfa72f48
Frestho <34761944+Frestho@users.noreply.github.com>
2018-01-20 17:15:04 -0800

    Now works

--- Dictionary Attack.py
+++ Dictionary Attack.py
@@ -13,19 +13,19 @@ while unsolved:
     attempts += 1
     wordOn[len(wordOn) - 1] += 1
     for i in reversed(range(1, len(wordOn))):
-        if wordOn[i] > len(words):
+        if wordOn[i] > len(words) - 1:
             wordOn[i] = 0
             wordOn[i - 1] += 1
     
     #adds another item to the end of the word list if the first word is "overflowed"
-    if wordOn[0] > len(words):
+    if wordOn[0] > len(words) - 1:
         wordOn[0] = 0
         wordOn.append(0)
     
     attempt = ""
-    for i in range (0, len(wordOn) - 1):
+    for i in range (0, len(wordOn)):
         attempt += words[wordOn[i]]
-    
+
     if attempt == pw:
-        print("Your password \"" + attempt + "\" has been found! It took " + str(attempts) + " attempts and " + str(round(time.time() - start), 2)) + " seconds."
+        print("Your password \"" + attempt + "\" has been found! It took " + str(attempts) + " attempts and " + str(round(time.time() - start, 2)) + " seconds.")
         unsolved = False
