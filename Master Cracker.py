272895cf0f68dc9eb7424850b8aceb327fc9c61c
Frestho <34761944+Frestho@users.noreply.github.com>
2018-01-21 21:20:50 -0800

    Somewhat working

--- Master Cracker.py
+++ Master Cracker.py
@@ -2,22 +2,32 @@
 import time
 pw = input("Enter your password: ")
 start = time.time()
-commonPws = open(commonpassword.txt).split()
-words = open(words.txt).split()
+commonPws = open("commonpasswords.txt").readlines()
+for i in range(0, len(commonPws)):
+    commonPws[i] = commonPws[i].rstrip()
+words = open("words.txt").readlines()
+for i in range(0, len(words)):
+    words[i] = words[i].rstrip()
+wordOn = [-1]
 totalAttempts = 0
 def common():
     for i in range(0, len(commonPws)):
-        
+        if commonPws[i] == pw:
+            print("Your password \"" + commonPws[i] + "\" has been found. It was number " + str(i + 1) + " on the most common password list.")
+            return
+    print("Password not found on the common password list, onto next method...")
 def bruteForce(mode, guesses):
+    global totalAttempts
     cracked = False #state of password cracking
     chars = mode #all the characters the the program will try
     charOn = [-1] #holds all the character values
     attempts = 0
     while not cracked:
         attempts += 1
-        totalAttempts += 1
         if attempts > guesses:
             print("Brute force failed, going on to next cracking method.")
+            return
+        totalAttempts += 1
         #changes the right-most character of the password that is being attempted
         charOn[len(charOn) - 1] += 1
         
@@ -38,11 +48,40 @@ def bruteForce(mode, guesses):
         #decodes charOn into a string and store it in attempt
         for i in range(0, len(charOn)):
             attempt += chars[charOn[i]]
-        
+
         #check if we got the password
         if attempt == pw:
             print("Your password was found! It is " + str(attempt) + ", right? It took me " + str(attempts) + " attempts to find it. It took " + str(round(time.time()-start, 2)) + " seconds.")
-            cracked = True
-def dict():
-    
+            return
+def dictionary(caps, guesses):
+    attempts = 0
+    unsolved = True
+    while unsolved:
+        attempts += 1
+        if attempts > guesses:
+            print("Brute force failed, going on to next cracking method.")
+        totalAttempts += 1
+        wordOn[len(wordOn) - 1] += 1
+        for i in reversed(range(1, len(wordOn))):
+            if wordOn[i] > len(words) - 1:
+                wordOn[i] = 0
+                wordOn[i - 1] += 1
         
+        #adds another item to the end of the word list if the first word is "overflowed"
+        if wordOn[0] > len(words) - 1:
+            wordOn[0] = 0
+            wordOn.append(0)
+        
+        attempt = ""
+        for i in range (0, len(wordOn)):
+            attempt += words[wordOn[i]]
+
+        if attempt == pw:
+            print("Your password \"" + attempt + "\" has been found! It took " + str(attempts) + " attempts and " + str(round(time.time() - start, 2)) + " seconds.")
+            unsolved = False
+def combo(guesses):
+    
+common()#attack that works often and takes less than a second, should definitely be done first
+dictionary(10000)#also short attack, guesses all one-word all-lowercase passwords
+bruteForce("1234567890", 13245678)#number passwords are not very common but hey, it doesn't take long to grind through 13245678 attempts
+bruteForce("aoeuidhtnspyfgcrlqjkxbmwvz1234567890", 10000000)#common password format
