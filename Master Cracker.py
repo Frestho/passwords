1824869dbb241d37f601dd8023cdb7b2c9670c15
Conics <34761944+Frestho@users.noreply.github.com>
2018-01-22 10:19:03 -0800

    Still not done

--- Master Cracker.py
+++ Master Cracker.py
@@ -59,7 +59,7 @@ def dictionary(caps, guesses):
     while unsolved:
         attempts += 1
         if attempts > guesses:
-            print("Brute force failed, going on to next cracking method.")
+            print("Dictionary attack failed, going on to next cracking method.")
         totalAttempts += 1
         wordOn[len(wordOn) - 1] += 1
         for i in reversed(range(1, len(wordOn))):
@@ -79,9 +79,41 @@ def dictionary(caps, guesses):
         if attempt == pw:
             print("Your password \"" + attempt + "\" has been found! It took " + str(attempts) + " attempts and " + str(round(time.time() - start, 2)) + " seconds.")
             unsolved = False
-def combo(guesses):
-    
+def combo(numbers, guesses):
+    wordOn = [-1]
+    #adds numbers if the bare dictionary attack isn't working
+    numbers = []
+    attempts = 0
+    unsolved = True
+    while unsolved: 
+        attempts += 1
+        if attempts > guesses:
+            print("Combination attack failed, going on to next cracking method.")
+        totalAttempt += 1
+        wordOn[len(wordOn) - 1] += 1
+        """
+        for i in reversed(range(1, len(wordOn)):
+            if wordOn[i] > len(words):
+                wordOn[i] = 0
+                wordOn[i - 1] += 1
+        """
+        #adds another item to the end of the word list if the first word is "overflowed"
+        if wordOn[0] > len(words) - 1:
+            wordOn[0] = 0
+            numbers.append(0)
+        if len(numbers) > 0:
+            numbers[len(numbers) - 1] += 1
+        attempt = ""
+        for i in range (0, len(wordOn)):
+            attempt += words[wordOn[i]]
+        if len(numbers) > 0:
+            attempt += str(numbers[0])
+        if attempt == pw:
+            print("Your password \"" + attempt + "\" has been found! It took " + str(attempts) + " attempts and " + str(round(time.time() - start, 2)) + " seconds to find your password.")
+            unsolved = False
 common()#attack that works often and takes less than a second, should definitely be done first
 dictionary(10000)#also short attack, guesses all one-word all-lowercase passwords
 bruteForce("1234567890", 13245678)#number passwords are not very common but hey, it doesn't take long to grind through 13245678 attempts
 bruteForce("aoeuidhtnspyfgcrlqjkxbmwvz1234567890", 10000000)#common password format
+combo(99, 1000000)
+bruteForce()
